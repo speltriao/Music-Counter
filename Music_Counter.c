35b16b432dd864430a07f0b47d4ecdb6ce94c2cb
speltriao <38846168+speltriao@users.noreply.github.com>
2021-01-16 19:32:45 -0300

    Add files via upload

--- Music_Counter.c
+++ Music_Counter.c
@@ -5,18 +5,27 @@
 #define limit 1000
 #define true 1
 #define false 0
-//COMPILED WITH GCC//
+
+//COLORS//
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
 
 static int count = 0; //Global variable for counting music files found
 
 void hello_msg(){
     printf("\n");
-    printf(" __  __           _          _____                  _    \n");
-    printf("|  \\/  |         (_)        / ____|                | |           \n");
-    printf("| \\  / |_   _ ___ _  ___   | |     ___  _   _ _ __ | |_ ___ _ __ \n");
-    printf("| |\\/| | | | / __| |/ __|  | |    / _ \\| | | | '_ \\| __/ _ \\ '__|\n");
-    printf("| |  | | |_| \\__ \\ | (__   | |___| (_) | |_| | | | | ||  __/ |   \n");
-    printf("|_|  |_|\\__,_|___/_|\\___|   \\_____\\___/ \\__,_|_| |_|\\__\\___|_|   \n");
+    printf(CYAN " __  __           _          _____                  _    "RESET "\n");
+    printf(GREEN "|  \\/  |         (_)        / ____|                | |           "RESET "\n");
+    printf(YELLOW "| \\  / |_   _ ___ _  ___   | |     ___  _   _ _ __ | |_ ___ _ __ "RESET "\n");
+    printf(BLUE "| |\\/| | | | / __| |/ __|  | |    / _ \\| | | | '_ \\| __/ _ \\ '__|"RESET "\n");
+    printf(MAGENTA "| |  | | |_| \\__ \\ | (__   | |___| (_) | |_| | | | | ||  __/ |   "RESET "\n");
+    printf(RED "|_|  |_|\\__,_|___/_|\\___|   \\_____\\___/ \\__,_|_| |_|\\__\\___|_|   "RESET "\n");
     printf("\n");
 
 
@@ -31,15 +40,18 @@ char* get_current_dir(){ //Return the current directory (where the program is)
 char* menu(){
     char in;
     static char cust_path[limit];
-    do
-    {
+    do{
         printf("Type 0 to use the current directory as base, or 1 to specify a path:\n");
         scanf(" %c",&in);
     }while ((in!='0')&&(in!='1'));
-    if (in=='0') return get_current_dir();
-    else if(in=='1') {
-        printf("Enter the desired path: ");
+
+    if (in=='0') {
         flush(stdin);
+        return get_current_dir();
+    }
+    else if(in=='1') {
+        flush(stdin);
+        printf("Enter the desired path: ");
         gets(cust_path);
         return(cust_path);
     }
@@ -76,23 +88,24 @@ void output(){
     printf("\nTotal of music found: %d\n\n",count);
 }
 
-void flush (FILE *in){
-    intch;
+void flush(FILE *in){
+    int ch;
     do{
-        ch=fgetc(in); 
-    }while (ch !=EOF && ch != '\n'); 
+        ch = fgetc (in); 
+    }while(ch != EOF && ch != '\n'); 
     clearerr(in);
 }
 
-void pause (){
-    flush(stdin);
-    printf("Press [Enter] to continue . . ." );
+void pause(){
+    
+    printf("Press [Enter] to continue . . .");
     flush(stdout);
     getchar();
     
 } 
 
-int main(void){
+int main(void)
+{
     hello_msg();
     list_files(menu());
     output();
